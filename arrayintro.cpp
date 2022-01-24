93fbebcafe694875d32d00f3283f47d9b90f2815
Md Azad Hossain Tutul <mdazadhossain95@gmail.com>
2022-01-24 16:30:02 +0600

    learn sum array

--- arrayintro.cpp
+++ arrayintro.cpp
@@ -7,12 +7,19 @@ int main()
     // a[4] = 5;
     // cout<<a[0]<<" "<<a[4]<<endl;
 
-    int a[10] = {1};
+    // int a[10] = {1, 2, 3, 4};
     // cout << a[0] << " " << a[1] << " " << a[2] << " " << a[3] << endl;
 
-    for(int i=0; i<10; i++){
+    // for(int i=0; i<10; i++){
+    //     cout<<a[i]<< " ";
+    // }
+
+    int a[10];
+    for(int i=0; i< 10; i++){
+        a[i] = i * i;
         cout<<a[i]<< " ";
     }
+
     
 
     return 0;
