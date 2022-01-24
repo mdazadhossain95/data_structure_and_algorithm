f9a880e6b6272c946f9d94b3017afc8eb71c03ce
Md Azad Hossain Tutul <mdazadhossain95@gmail.com>
2022-01-24 23:13:21 +0600

    revarse array learned

--- revarse_array_print.cpp
+++ revarse_array_print.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 int main()
 {
+
     int n;
     cin >> n;
-
     int a[n];
+
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-    //normal outbut
-    cout << "before revarse : " << endl;
+
+    cout << endl;
+
+    //normal order
     for (int i = 0; i < n; i++)
     {
         cout << a[i] << " ";
@@ -19,22 +22,10 @@ int main()
 
     cout << endl;
 
-    //revarse array
-    int start = 0;
-    int end = n - 1;
-    while (start <= end)
-    {
-        swap(a[start], a[end]);
-
-        start++;
-        end--;
-    }
-
-    cout << "After revarse : " << endl;
-    for (int i = 0; i < n; i++)
+    //reverse order
+    for (int i = 4 - 1; i >= 0; i--)
     {
         cout << a[i] << " ";
     }
-
     return 0;
 }
