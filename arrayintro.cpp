42a7d1ea3713de0174d8bd7896882d364f0f040b
Md Azad Hossain Tutul <mdazadhossain95@gmail.com>
2022-01-24 15:07:01 +0600

    first commit

---
+++ arrayintro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a[10];
+    // a[0] = 2;
+    // a[4] = 5;
+    // cout<<a[0]<<" "<<a[4]<<endl;
+
+    int a[10] = {1};
+    // cout << a[0] << " " << a[1] << " " << a[2] << " " << a[3] << endl;
+
+    for(int i=0; i<10; i++){
+        cout<<a[i]<< " ";
+    }
+    
+
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    printf("Hello World");
+    printf("This is From VSCode");
+    int var;
+    cin>> var;
+    cout<< var;
+    // printf(var);
+
+    }
