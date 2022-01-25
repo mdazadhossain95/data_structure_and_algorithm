870e357e0129b13f0e4da54eabfc0736f38c9d4f
Md Azad Hossain Tutul <mdazadhossain95@gmail.com>
2022-01-25 13:51:26 +0600

    changed name of files

--- strings/cin_getline.cpp
+++ strings/cin_getline.cpp
@@ -5,7 +5,7 @@ int main()
     //by using getline we can write string with space, tabs or new lines
     char name[100];
     cout << "Enter your name : ";
-    cin.getline(name, 100);
+    cin.getline(name, 100, 'o');
     cout << "Your name is " << name << endl;
 
     return 0;
